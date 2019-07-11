2857b6ff8b53060f72ae98a81f2db77c6df2d46c
Karan Sekhri <ksekhri@gmail.com>
2019-07-10 23:23:20 -0700

    Configure file-loader in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,16 @@ module.exports = {
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
-      }
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
