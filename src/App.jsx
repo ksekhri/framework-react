2e35b121552f99724245587c1fc30d4311dc028a
Karan Sekhri <ksekhri@gmail.com>
2019-07-10 23:23:36 -0700

    Add and load test image

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
 import './App.css';
+import reactIcon from './assets/images/react-icon.svg';
 
 function App() {
   return(
     <div className="App">
       <h1>React App</h1>
+      <img src={reactIcon} height="200px" width="200px" />
     </div>
   );
 }
